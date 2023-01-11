d28190d9f22b4d072af4de894ccdbb0e6ff5ee89
TetianaAsadova <114417194+TetianaAsadova@users.noreply.github.com>
2023-01-11 22:06:01 +0200

    s12

--- js/exampl.js
+++ js/exampl.js
@@ -1,14 +1,22 @@
-function createArrayOfNumbers(min, max) {
-  const numbers = [];
-  // Change code below this line
-  let member = min;
-  
-  for (let i = 0; i <= max - min; i += 1) {
-    numbers = numbers.push(member);
-    member += 1;
-  }
-  // Change code above this line
-  return numbers;
-}
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
 
-console.log(createArrayOfNumbers(3, 7));
+propName = 'name';
+  // Change code below this line
+  const keyValue = [];
+  
+  for (const product of products) {     
+    keyValue.push(product[propName]); 
+    console.log(keyValue);
+}
+console.log(product);
+  console.log(keyValue);
+  // Change code above this line
+
+
+// getAllPropValues("name");
+
