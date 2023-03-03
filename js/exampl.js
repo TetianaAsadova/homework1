84fd412ff35ea2f866e083efd08d776633038e47
TetianaAsadova <114417194+TetianaAsadova@users.noreply.github.com>
2023-03-03 17:42:04 +0200

    Update exampl.js
    sum1

--- js/exampl.js
+++ js/exampl.js
@@ -1,10 +1,29 @@
 const atTheOldToad = {
+  potions: [
+    { name: "Speed potion", price: 460 },
+    { name: "Dragon breath", price: 780 },
+    { name: "Stone skin", price: 520 },
+  ],
   // Change code below this line
-  items: [],
-  potions() {
-     return this.items;
+  getPotions() {
+    return this.potions;
   },
-  // Change code above this line
-};
 
-console.log(atTheOldToad.potions());
+  updatePotionName(oldName, newName) {
+    for (let i = 0; i < this.potions.length; i += 1) {
+      console.log("this.potions[i].name", this.potions[i].name);
+      console.log("oldName", oldName);
+      if (this.potions[i].name === oldName) {
+        this.potions[i].name = newName;
+        console.log(`$(new this.potions[i].name)`, this.potions[i].name);
+        console.log(this.potions)
+        break;
+      }
+      
+    }
+     console.log(`Potion ${oldName} is not in inventory!`) ;
+  },  
+};
+atTheOldToad.updatePotionName("Stone skin", "Invulnerability potion");
+atTheOldToad.updatePotionName("Dragon breath", "Polymorth");
+atTheOldToad.updatePotionName("Dragonbreath", "Polymorth");
